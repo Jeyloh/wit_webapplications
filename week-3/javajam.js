32635543ac27a90e2e504bf0434f42613a1349fc
unknown <20074232@IT101PC06.wit.ie>
2016-09-27 12:13:54 +0100

    added week 2 and 3 practicals 'javajam coffeehouse'

---
+++ week-3/javajam.js
@@ -0,0 +1,11 @@
+/* Toggle the "active" and "show" classes when the user click an element related to the class "accordion"
+*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
